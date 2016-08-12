3ef5e244e625f989265d3f9ddf36b778c3246ff2
Ryan Collins <rccproductions@mac.com>
2016-08-11 22:50:03 -0400

    Chore: adjust deploy.py script

--- server/deploy.py
+++ server/deploy.py
@@ -7,8 +7,25 @@ call(["npm", "run", "deploy"])
 time.sleep(120)
 src = './app/dist'
 dest = './server/public'
-src_files = os.listdir(src)
-for file_name in src_files:
-    full_file_name = os.path.join(src, file_name)
-    if (os.path.isfile(full_file_name)):
-        shutil.move(full_file_name, dest)
+
+def clearOldFiles(dir):
+    old_files = os.listdir(dir)
+    for file_name in old_files:
+        full_file_name = os.path.join(dir, file_name)
+        if (os.path.isfile(full_file_name)):
+            os.remove(full_file_name)
+
+
+def moveNewFiles(dir):
+    for file_name in os.listdir(dir):
+        full_file_name = os.path.join(src, file_name)
+        if (os.path.isfile(full_file_name)):
+            shutil.move(full_file_name, dest)
+
+
+def main():
+    clearOldFiles(dest)
+    moveNewFiles(src)
+
+
+main()
