50f79c3c677f4badb715edfe96234174b3e7f5cf
Ryan Collins <rccproductions@mac.com>
2016-08-15 17:37:35 -0400

    Fix: make the hero carousel component a class to deal with grommet bugs

--- app/src/components/HeroCarousel/index.js
+++ app/src/components/HeroCarousel/index.js
@@ -1,4 +1,4 @@
-import React, { PropTypes } from 'react';
+import React, { PropTypes, Component } from 'react';
 import styles from './index.module.scss';
 import cssModules from 'react-css-modules';
 import {
@@ -7,34 +7,39 @@ import {
   Headline,
 } from 'grommet';
 
-const HeroCarousel = ({
-  restaurants,
-}) => (
-  <div className={styles.heroCarousel}>
-    <div className={styles.overlay}>
-      <Headline className={styles.headline} align="center" size="large">
-        Restaurant Reviewer
-      </Headline>
-      <Headline className={styles.headline} align="center" size="small">
-        Accessibility App
-      </Headline>
-      <Heading className={styles.subHeadline} align="center" tag="h6">
-        by <a className={styles.link} href="http://www.ryancollins.io">Ryan Collins</a>
-      </Heading>
-    </div>
-    <Carousel>
-      {restaurants.map((item, index) =>
-        <div key={index} className={styles.carousel}>
-          <img
-            src={item.src}
-            alt={item.caption}
-            className={styles.image}
-          />
+class HeroCarousel extends Component {
+  render() {
+    const {
+      restaurants,
+    } = this.props;
+    return (
+      <div className={styles.heroCarousel}>
+        <div className={styles.overlay}>
+          <Headline className={styles.headline} align="center" size="large">
+            Restaurant Reviewer
+          </Headline>
+          <Headline className={styles.headline} align="center" size="small">
+            Accessibility App
+          </Headline>
+          <Heading className={styles.subHeadline} align="center" tag="h6">
+            by <a className={styles.link} href="http://www.ryancollins.io">Ryan Collins</a>
+          </Heading>
         </div>
-      )}
-    </Carousel>
-  </div>
-);
+        <Carousel>
+          {restaurants.map((item, index) =>
+            <div key={index} className={styles.carousel}>
+              <img
+                src={item.src}
+                alt={item.caption}
+                className={styles.image}
+              />
+            </div>
+          )}
+        </Carousel>
+      </div>
+    );
+  }
+}
 
 HeroCarousel.propTypes = {
   restaurants: PropTypes.array.isRequired,
