9804402855a1a5f11de3f6a388d0353a91b6a2a9
Ryan Collins <rccproductions@mac.com>
2016-09-01 21:51:24 -0400

    Chore: add production bundle.js

--- app/src/components/AppFooter/tests/index.test.js
+++ app/src/components/AppFooter/tests/index.test.js
@@ -1,5 +1,4 @@
 import AppFooter from '../index';
-
 import expect from 'expect';
 import { shallow } from 'enzyme';
 import React from 'react';
@@ -7,5 +6,11 @@ import React from 'react';
 describe('<AppFooter />', () => {
   it('Expect to have unit tests specified', () => {
     expect(true).toEqual(false);
+  });
+  it('Has a menu item', () => {
+    expect(true).toEqual(true);
+  });
+  it('Has social sharing links', () => {
+    expect(true).toEqual(true);
   });
 });
