48a50bf476396d3dd3d6220d55cd72024da50938
Ryan Collins <rccproductions@mac.com>
2016-09-05 14:20:20 -0400

    Chore: add production bundle.js and also setup deploy to go to server folder

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -86,7 +86,8 @@ module.exports = {
     },
   },
   output: {
-    path: process.env.NODE_ENV === 'production' ? path.resolve(ROOT_PATH, 'app/dist') : path.resolve(ROOT_PATH, 'app/build'),
+    path: process.env.NODE_ENV === 'production' ?
+      path.resolve(ROOT_PATH, 'server/public') : path.resolve(ROOT_PATH, 'app/build'),
     publicPath: '/',
     filename: 'bundle.js',
   },
